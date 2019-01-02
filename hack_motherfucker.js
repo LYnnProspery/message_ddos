411d5b3cb18e6ce80418e3e155e0c57d63663f9a
iamlynn <lynnprosper@163.com>
2019-01-02 17:26:30 +0800

    Update hack_motherfucker.js

--- hack_motherfucker.js
+++ hack_motherfucker.js
@@ -31,59 +31,6 @@ let hach_motherfucker = () => {
         console.log(err)
     })
 
-    // axios({
-    //     url: 'https://api.miaodiyun.com/20150822/industrySMS/sendSMS',
-    //     method: 'post',
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded'
-    //     },
-    //     data: {
-    //         accountSid: accountSid,
-    //         smsContent: '尊敬的用户，您的验证码为: {1}，祝您早日康复',
-    //         templateid: '285661450',
-    //         to: '13521645433',
-    //         // to: testNumber,
-    //         timestamp: timestamp,
-    //         sig: crypto.createHash('md5').update(accountSid + token + timestamp).digest('hex'),
-    //         param: '444444'
-    //     }
-    // }).then(res => {
-    //     console.log(res.data)
-    //     console.log(++count, 'send ' + count)
-    // }).catch(err => {
-    //     console.log(err)
-    // })
-
-    // request({
-    //     url: 'https://api.miaodiyun.com/20150822/industrySMS/sendSMS',
-    //     method: 'post',
-    //     headers: {
-    //         'Content-type': 'application/x-www-form-urlencoded'
-    //     },
-    //     data: {
-    //         accountSid: accountSid,
-    //         smsContent: '尊敬的用户，您的验证码为: 36791623，祝您早日康复',
-    //         templateid: 'hack_motherfucker2',
-    //         // to: '13521645433',
-    //         to: testNumber,
-    //         timestamp: timestamp,
-    //         sig: crypto.createHash('md5').update(accountSid + token + timestamp).digest('hex')
-    //     }
-    // }, (res) => {
-    //     console.log(res)
-    // })
-    // request.post('https://api.miaodiyun.com/20150822/industrySMS/sendSMS', {form: {
-    //     accountSid: accountSid,
-    //         smsContent: '尊敬的用户，您的验证码为: {1}，祝您早日康复',
-    //         templateid: '285661450',
-    //         // to: '13521645433',
-    //         to: testNumber,
-    //         timestamp: timestamp,
-    //         sig: crypto.createHash('md5').update(accountSid + token + timestamp).digest('hex'),
-    //         param: '444444'
-    // }}, (res) => {
-    //     console.log(res)
-    // })
 
 }
 
